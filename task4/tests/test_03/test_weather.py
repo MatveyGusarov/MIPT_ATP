3c53a5423e67773833e2f521464c986999b8dbe4
MatveyGusarov <gusarov.mm@phystech.edu>
2022-05-12 21:49:34 +0300

    debug weather

--- task4/tests/test_03/test_weather.py
+++ task4/tests/test_03/test_weather.py
@@ -8,6 +8,10 @@ KEY = '197fc3253b8cedb3fa5f3bb170577c51'
 request = Mocker()
 weather = WeatherWrapper(KEY)
 
+def test_response_city():
+    request.get(BASE_URL, status_code=404)
+    assertRaises(AttributeError, weather.get_response_city, 'Abracadabracity', BASE_URL)
+
 def test_all():       
     request.get(BASE_URL, status_code=200, json={'main': {'temp': 0}})
     request.get(FORECAST_URL, status_code=200, json={'list': [{'main': {'temp': 0}}] * 8})
@@ -21,7 +25,4 @@ def test_all():
 
     assert weather.find_diff_two_cities('Barcelona', 'Astana') > 0
     assert weather.get_diff_string('Barcelona', 'Astana') == 'Weather in Barcelona is warmer than in Astana by 5 degrees'
-    
-    request.get(BASE_URL, status_code=404)
-    unittest.TestCase.assertRaises(AttributeError, weather.get_response_city, 'Abracadabracity', BASE_URL)
                                                                                                                         
