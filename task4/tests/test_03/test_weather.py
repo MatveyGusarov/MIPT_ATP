5e0913c03ad0d2d0a5564b1891c87f2145c5b398
MatveyGusarov <gusarov.mm@phystech.edu>
2022-05-13 19:31:47 +0300

    debug weather

--- task4/tests/test_03/test_weather.py
+++ task4/tests/test_03/test_weather.py
@@ -11,19 +11,15 @@ def test_all(requests_mock):
     requests_mock.get(BASE_URL, status_code=200, json={'main': {'temp': 0}})
     requests_mock.get(FORECAST_URL, status_code=200, json={'list': [{'main': {'temp': 0}}] * 8})
 
-    assert weather.get_tomorrow_diff('Barcelona') == 'The weather in Barcelona tomorrow will be warmer than today'
+    assert weather.get_tomorrow_diff('Barcelona') == 'The weather in Barcelona tomorrow will be the same than today'
 
     requests_mock.get(FORECAST_URL, status_code=200, json={'list': [{'main': {'temp': 2}}] * 8})
-    assert weather.get_tomorrow_diff('Tomsk') == 'The weather in Tomsk tomorrow will be much warmer than today'
+    assert weather.get_tomorrow_diff('Tomsk') == 'The weather in Tomsk tomorrow will be warmer than today'
 
     requests_mock.get(FORECAST_URL, status_code=200, json={"list": [{'main': {'temp': -2}}] * 8})
     assert weather.get_tomorrow_diff('Kiev') == 'The weather in Kiev tomorrow will be colder than today'
 
     assert weather.find_diff_two_cities('Barcelona', 'Astana') > 0
     assert weather.get_diff_string('Barcelona', 'Astana') == 'Weather in Barcelona is warmer than in Astana by 6 degrees'
-    
-class WeatherTests(unittest.TestCase):
-    def test_get_response_city(self, requests_mock):
-        requests_mock.get(BASE_URL, status_code=404)
-        self.assertRaises(AttributeError, weather.get_response_city, 'Abracadabracity', BASE_URL)
-                                                                                                                        
+    with pytest.raises(AttributeError) as e_info:
+    	weather_forecast.get("NotExistedCity", FORECAST_URL)	
