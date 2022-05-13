f558011c0931506c34db5b0cbf531b9e2c371db9
MatveyGusarov <gusarov.mm@phystech.edu>
2022-05-13 19:17:28 +0300

    debug weather

--- task4/tests/test_03/test_weather.py
+++ task4/tests/test_03/test_weather.py
@@ -5,26 +5,25 @@ from weather_03.weather_wrapper import BASE_URL, FORECAST_URL, WeatherWrapper
 
 KEY = '197fc3253b8cedb3fa5f3bb170577c51'
 
-request = Mocker()
 weather = WeatherWrapper(KEY)
 
-def test_all():       
-    request.get(BASE_URL, status_code=200, json={'main': {'temp': 0}})
-    request.get(FORECAST_URL, status_code=200, json={'list': [{'main': {'temp': 0}}] * 8})
+def test_all(requests_mock):       
+    requests_mock.get(BASE_URL, status_code=200, json={'main': {'temp': 0}})
+    requests_mock.get(FORECAST_URL, status_code=200, json={'list': [{'main': {'temp': 0}}] * 8})
 
     assert weather.get_tomorrow_diff('Barcelona') == 'The weather in Barcelona tomorrow will be warmer than today'
 
-    request.get(FORECAST_URL, status_code=200, json={'list': [{'main': {'temp': 2}}] * 8})
+    requests_mock.get(FORECAST_URL, status_code=200, json={'list': [{'main': {'temp': 2}}] * 8})
     assert weather.get_tomorrow_diff('Tomsk') == 'The weather in Tomsk tomorrow will be much warmer than today'
 
-    request.get(FORECAST_URL, status_code=200, json={"list": [{'main': {'temp': -2}}] * 8})
+    requests_mock.get(FORECAST_URL, status_code=200, json={"list": [{'main': {'temp': -2}}] * 8})
     assert weather.get_tomorrow_diff('Kiev') == 'The weather in Kiev tomorrow will be colder than today'
 
     assert weather.find_diff_two_cities('Barcelona', 'Astana') > 0
     assert weather.get_diff_string('Barcelona', 'Astana') == 'Weather in Barcelona is warmer than in Astana by 6 degrees'
     
 class WeatherTests(unittest.TestCase):
-    def test_get_response_city(self):
-        request.get(BASE_URL, status_code=404)
+    def test_get_response_city(self, requests_mock):
+        requests_mock.get(BASE_URL, status_code=404)
         self.assertRaises(AttributeError, weather.get_response_city, 'Abracadabracity', BASE_URL)
                                                                                                                         
